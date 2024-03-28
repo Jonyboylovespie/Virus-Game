d8a111b73ce7dc5ff1d82e9b087ad497e49169d3
Jonyboylovespie <jonathan.frawley@outlook.com>
2024-03-28 17:04:10 -0400

    Update CameraFollow.cs

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,22 +3,21 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
     public float smoothSpeed = 0.125f;
-    public Vector3 offset;
     public Transform playerTransform;
 
     private void Start()
     {
         playerTransform = GameObject.Find("Player").transform;
+        transform.position = playerTransform.position;
     }
 
     void FixedUpdate()
     {
         Vector3 desiredPosition = playerTransform.position;
-        desiredPosition.y = 0;
-        desiredPosition.z = -10;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        smoothedPosition.y = 0;
+        smoothedPosition.z = -10;
         transform.position = smoothedPosition;
     }
 }
